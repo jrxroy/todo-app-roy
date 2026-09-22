8483fe04a9ab4915f444b450fc9582f1fbe4d230
Roy Simarmata <jrxroy@gmail.com>
2026-09-22 11:30:34 +0700

    fix: remove pwa config for smooth vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // PWA aktif saat production build
-});
+import type { NextConfig } from "next";
 
-const nextConfig = {};
+const nextConfig: NextConfig = {};
 
-module.exports = withPWA(nextConfig);
+export default function(phase: string, { defaultConfig }: { defaultConfig: NextConfig }) {
+  return nextConfig;
+}
